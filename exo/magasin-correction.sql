fcce19e667a747fa6a53c34c9e445b5319b4e053
3Z33 <144033480+3Z33@users.noreply.github.com>
2023-12-15 16:35:04 +0100

    test

--- exo/magasin-correction.sql
+++ exo/magasin-correction.sql
@@ -21,7 +21,7 @@ Id est primaire en incrémentation automatique
 Une table produit avec 4 colonnes
 id int
 nom string
-quantité nombre à virgule
+quantité int
 prix nombre à virgule
 Id est primaire en incrémentation automatique
 
@@ -50,8 +50,17 @@ CREATE TABLE commande (
     client_id INT(11) NOT NULL,
     date_achat DATETIME NOT NULL,
     reference VARCHAR(255) NOT NULL,
-    prix_total FLOAT NOT NULL 
+    prix_total FLOAT NOT NULL,
+    CONSTRAINT ClientExist FOREIGN KEY (client_id) REFERENCES client (id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
 );
+
+ALTER TABLE commande  
+    ADD CONSTRAINT ClientExist FOREIGN KEY (client_id) 
+    REFERENCES client (id)
+    ADD CONSTRAINT ProduitExist FOREIGN KEY (reference)
+    REFERENCES produit (id);
 
 INSERT INTO commande (client_id, date_achat, reference, prix_total) VALUES
 (1, '2023-12-12 10:10:10', '001471', 100),
@@ -66,3 +75,16 @@ INSERT INTO commande (client_id, date_achat, reference, prix_total) VALUES
 (1, '2023-12-12 10:10:10', '001471', 100),
 (1, '2023-12-12 10:10:10', '001471', 100),
 (1, '2023-12-12 10:10:10', '001471', 100);
+
+
+CREATE TABLE IF NOT EXISTS produit (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    quantite INT(11) NOT NULL,
+    prix FLOAT NOT NULL
+);
+
+INSERT INTO produit (nom, quantite, prix) VALUES 
+('Pomme', 10, 50),
+('Casse Noisette', 100, 100),
+('Noisette', 1000, 2);
